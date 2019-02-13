a81023a33b9d130cb120e93378ff649021124e9d
Mario Galvan <15mgm15@gmail.com>
2019-02-13 13:35:38 -0600

    [Feature] - First commit.

--- VisualChallenge/VisualChallenge.Android/Resources/Resource.designer.cs
+++ VisualChallenge/VisualChallenge.Android/Resources/Resource.designer.cs
@@ -5604,26 +5604,26 @@ namespace VisualChallenge.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020138
-			public const int avd_hide_password_1 = 2130837816;
-			
 			// aapt resource value: 0x7f020139
-			public const int avd_hide_password_2 = 2130837817;
+			public const int avd_hide_password_1 = 2130837817;
 			
 			// aapt resource value: 0x7f02013a
-			public const int avd_hide_password_3 = 2130837818;
+			public const int avd_hide_password_2 = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int avd_hide_password_3 = 2130837819;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_show_password_1 = 2130837819;
-			
 			// aapt resource value: 0x7f02013c
-			public const int avd_show_password_2 = 2130837820;
+			public const int avd_show_password_1 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int avd_show_password_3 = 2130837821;
+			public const int avd_show_password_2 = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int avd_show_password_3 = 2130837822;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -6282,11 +6282,11 @@ namespace VisualChallenge.Droid
 			// aapt resource value: 0x7f020130
 			public const int notification_icon_background = 2130837808;
 			
-			// aapt resource value: 0x7f020136
-			public const int notification_template_icon_bg = 2130837814;
-			
 			// aapt resource value: 0x7f020137
-			public const int notification_template_icon_low_bg = 2130837815;
+			public const int notification_template_icon_bg = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int notification_template_icon_low_bg = 2130837816;
 			
 			// aapt resource value: 0x7f020131
 			public const int notification_tile_bg = 2130837809;
@@ -6295,13 +6295,16 @@ namespace VisualChallenge.Droid
 			public const int notify_panel_notification_icon_bg = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int tooltip_frame_dark = 2130837811;
+			public const int rxv_logo_white = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int tooltip_frame_light = 2130837812;
+			public const int tooltip_frame_dark = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int xamagon_preview = 2130837813;
+			public const int tooltip_frame_light = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int xamagon_preview = 2130837814;
 			
 			static Drawable()
 			{
