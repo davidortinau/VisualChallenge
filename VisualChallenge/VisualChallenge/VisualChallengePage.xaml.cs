f944d27590ba0659ebe07405bef7d99213c8fee0
Matthew Leibowitz <mattleibow@live.com>
2019-02-16 13:33:28 +0200

    First attempt at recreating the mixer Android app

--- VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
+++ VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
@@ -1,20 +1,109 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace VisualChallenge
 {
-    public partial class VisualChallengePage : ContentPage
-    {
-        public VisualChallengePage()
-        {
-            InitializeComponent();
-        }
+	public partial class VisualChallengePage : ContentPage
+	{
+		private double carouselItemHeight;
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            Device.OpenUri(new Uri("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/visual"));
-        }
-    }
+		public VisualChallengePage()
+		{
+			InitializeComponent();
+
+			CarouselItems = new CarouselItem[]
+			{
+				new CarouselItem {
+					Thumbnail = "thumbnail1.jpg",
+                    Avatar = "https://mixer.com/api/v1/users/230928/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail2.jpg",
+                    Avatar = "https://mixer.com/api/v1/users/59366910/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail3.jpg",
+				    Avatar = "https://mixer.com/api/v1/users/21101171/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail4.jpg",
+					Avatar = "https://mixer.com/api/v1/users/230928/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail5.jpg",
+					Avatar = "https://mixer.com/api/v1/users/59366910/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail6.jpg",
+					Avatar = "https://mixer.com/api/v1/users/21101171/avatar?w=128&h=128"
+				},
+				new CarouselItem {
+					Thumbnail = "thumbnail7.jpg",
+					Avatar = "https://mixer.com/api/v1/users/230928/avatar?w=128&h=128"
+				}
+			};
+
+			BindingContext = this;
+
+			SizeChanged += OnSizeChanged;
+		}
+
+		public IEnumerable<CarouselItem> CarouselItems { get; }
+
+		public double CarouselItemHeight
+		{
+			get => carouselItemHeight;
+			private set
+			{
+				carouselItemHeight = value;
+				OnPropertyChanged(nameof(CarouselItemHeight));
+			}
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			OnSizeChanged(this, EventArgs.Empty);
+		}
+
+		private void OnSizeChanged(object sender, EventArgs e)
+		{
+			var itemWidth = Width * 0.6;
+			CarouselItemHeight = itemWidth * 0.8;
+
+			foreach (var item in CarouselItems)
+			{
+				item.ImageWidth = itemWidth;
+			}
+		}
+	}
+
+	public class CarouselItem : BindableObject
+	{
+		private static Random rnd = new Random();
+
+		private double imageWidth;
+
+		public string UserName { get; set; } = "CoolUserName";
+
+		public string GameName { get; set; } = "The Game";
+
+		public int ViewCount { get; set; } = rnd.Next(999);
+
+		public string Thumbnail { get; set; }
+
+        public string Avatar { get; set; }
+
+		public double ImageWidth
+		{
+			get => imageWidth;
+			set
+			{
+				imageWidth = value;
+				OnPropertyChanged(nameof(ImageWidth));
+			}
+		}
+	}
 }
