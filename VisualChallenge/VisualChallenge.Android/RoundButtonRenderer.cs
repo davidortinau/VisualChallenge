ee5cf4426e01022168dbd5b275605188d1c9f198
Ben Buttigieg <bebuttig@microsoft.com>
2019-03-21 17:05:41 +0000

    MixRadio Player using Xamarin Visual

---
+++ VisualChallenge/VisualChallenge.Android/RoundButtonRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(VisualChallenge.Android.RoundButtonRenderer), new[] { typeof(VisualChallenge.RoundButtonVisual) })]
+namespace VisualChallenge.Android
+{
+    public class RoundButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                // Cleanup
+            }
+
+            if (e.NewElement != null)
+            {
+               Control.Background = null; // Required on Android otherwise button has a square background.
+                //UIImage test = UIImage.FromFile("Hate.png");
+
+                //Control.SetBackgroundImage(test, UIControlState.Normal);
+                //Control.SetBackgroundImage(UIImage.FromFile("Love.png"), UIControlState.Selected);
+
+                //Control.SetTitleShadowColor(Color.Black.ToUIColor(), UIKit.UIControlState.Normal);
+            }
+        }
+    }
+}
