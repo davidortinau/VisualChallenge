1d1acb25711510c1e593d9ee2da532aa8cdd6e3c
Bruno Caceiro <bcaceiro@gmail.com>
2019-03-25 15:49:00 +0000

    Started Visual Challenge layout

--- VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
+++ VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
+using VisualChallenge.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,9 +8,47 @@ namespace VisualChallenge
 {
     public partial class VisualChallengePage : ContentPage
     {
+        private ObservableCollection<Item> _items = new ObservableCollection<Item>();
+        public ObservableCollection<Item> Items
+        {
+            get => _items;
+            set => _items = value;
+        }
+
+
+
+
+
         public VisualChallengePage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+
+
+
+            Items.Add(new Item
+            {
+                Id = "1",
+                Status ="Open",
+                Subject = "Subject 123"
+            });
+
+
+            Items.Add(new Item
+            {
+                Id = "2",
+                Status = "Open",
+                Subject = "aaaaaaa cvvvvv"
+            });
+
+
+            Items.Add(new Item
+            {
+                Id = "3",
+                Status = "Open",
+                Subject = "Subject aaaaaa"
+            });
         }
 
         private void Button_Clicked(object sender, EventArgs e)
