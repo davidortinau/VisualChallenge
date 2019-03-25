170c188efeba621554d3c1c7e27db8436282b316
Mike Parker <mikeparker@mikes-mbp.home>
2019-03-25 18:13:16 +0000

    Initial time-boxed implementation of social image sharing style app UI

--- VisualChallenge/VisualChallenge/AppShell.xaml.cs
+++ VisualChallenge/VisualChallenge/AppShell.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace VisualChallenge
@@ -10,6 +10,15 @@ namespace VisualChallenge
         public AppShell()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigating(ShellNavigatingEventArgs args)
+        {
+            base.OnNavigating(args);
+
+            // Cancel all navigation for demo purposes
+            if (CurrentItem != null)
+                args.Cancel();
         }
     }
 }
---
+++ VisualChallenge/VisualChallenge/PostData.cs
@@ -0,0 +1,16 @@
+﻿namespace VisualChallenge
+{
+    // Quick and direct structure to support CollectionView visuals
+    public class PostData
+    {
+        public string Username { get; set; }
+        public string PostTitle { get; set; }
+        public string PostDetail { get; set; }
+        public string PostImage { get; set; }
+        public string LikeCountText { get; set; }
+        public string CommentCountText { get; set; }
+        public string TimestampText { get; set; }
+        public string UserImage => $"{Username}.jpg";
+        public string PostImageUrl => $"{PostImage}.jpg";
+    }
+}
