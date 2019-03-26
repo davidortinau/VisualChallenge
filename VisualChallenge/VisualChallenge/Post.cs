e45c323074f2db886aed8e247a0b1c6a4d7c1613
Alex Blount <alexkblount@gmail.com>
2019-03-26 09:04:20 -0700

    LinkedIn Visual Challenge entry

---
+++ VisualChallenge/VisualChallenge/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace VisualChallenge
+{
+    public class Post
+    {
+        public string ProfileImageUrl { get; set; }
+
+        public string ProfileName { get; set; }
+
+        public string ProfileJobTitle { get; set; }
+
+        public string PostedAgo { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string LikesName { get; set; }
+
+        public int LikesCount { get; set; }
+
+        public int CommentsCount { get; set; }
+
+    }
+}
--- VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
+++ VisualChallenge/VisualChallenge/VisualChallengePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,14 +8,78 @@ namespace VisualChallenge
 {
     public partial class VisualChallengePage : ContentPage
     {
+
+        public string UserProfileImageUrl { get; set; }
+
+        public List<Post> Posts { get; set; }
+
         public VisualChallengePage()
         {
             InitializeComponent();
+
+
+            Posts = new List<Post>
+            {
+                new Post
+                {
+                    ProfileName = "David Ortinau",
+                    ProfileImageUrl = "https://lh5.googleusercontent.com/-tACrxTCkr4w/AAAAAAAAAAI/AAAAAAAAABw/gQNWYdrs0CE/photo.jpg",
+                    PostedAgo = "5d",
+                    ProfileJobTitle = "Senior Program Manager, Mobile Dev Tools at Microsoft. Xamarin Mobile SDKs.",
+                    Description = "flurry of entries for the #XamarinForms Visual Challenge. Thanks for helping us improve things! Latest submissions from: @brunocaceiro @nitescua @Chard003 djlorent @Cheesebaron HobDev",
+                    ImageUrl = "https://pbs.twimg.com/media/DYGwImfVAAE1cHJ.jpg",
+                    LikesName = "Chris Hardy",
+                    CommentsCount = 3,
+                    LikesCount = 15
+                },
+
+                new Post
+                {
+                    ProfileName = "Pierce Boggan",
+                    ProfileImageUrl = "https://cdn-images-1.medium.com/max/1200/0*bbs98lWH7s9jC73k.",
+                    PostedAgo = "6h",
+                    ProfileJobTitle = "Program Manager Manager at Microsoft",
+                    Description = "Excellent blog sharing some of the tips Microsoft's mobile customer advisory team has learned working with countless customers. Looking forward to more posts like this!",
+                    ImageUrl = "https://pbs.twimg.com/media/DXs0GKIVAAEwFCZ.jpg:large",
+                    LikesName = "Alex Blount",
+                    CommentsCount = 3,
+                    LikesCount = 15
+                },
+
+                new Post
+                {
+                    ProfileName = "Chris Hardy",
+                    ProfileImageUrl = "http://thetabletshow.com/dnr_photos/chrishardy.jpg",
+                    PostedAgo = "6h",
+                    ProfileJobTitle = "Program Manager Manager at Microsoft ",
+                    Description = "As part of the RBC Innovation & Entrepreneurship Speaker Series, I spoke with Michael Katchen Co-founder of Wealthsimple. The Q&A was part of UofT’s 3rd annual Entrepreneurship Week, the University of Toronto’s celebration of “all things entrepreneurship.” RBC is partners with UofT’s hashtag#ONRamp & Creative Destruction Lab & supports this series as well as the RBC Innovation Prize.",
+                    ImageUrl = "https://blog.xamarin.com/wp-content/uploads/2016/05/Nat-keynote.jpg",
+                    LikesName = "Pierce Boggan",
+                    CommentsCount = 3,
+                    LikesCount = 15
+                },
+
+                new Post
+                {
+                    ProfileName = "Michael Ridland",
+                    ProfileImageUrl = "https://media.licdn.com/dms/image/C4E03AQGqQp0kKOGgHQ/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=bevOW2245NyJ_CqWkjFqDizgUdf5lYpXBTXZRtQDX-4",
+                    PostedAgo = "1mo",
+                    ProfileJobTitle = "Father, Technical Director, Founder",
+                    Description = "If your at Microsoft Ignite come past demo station 5. I’ll be giving a demo of building serverless and real-time systems using Cosmos DB, Functions and SignalR service.",
+                    ImageUrl = "https://blog.xamarin.com/wp-content/uploads/2016/02/University.jpg",
+                    LikesName = "David Ortinau",
+                    CommentsCount = 3,
+                    LikesCount = 15
+                }
+            };
+
+            BindableLayout.SetItemsSource(ContentLayout, Posts);
+
+            UserProfileImage.Source = "https://media.licdn.com/dms/image/C5603AQFQrLa724fv2A/profile-displayphoto-shrink_200_200/0?e=1559174400&v=beta&t=cTbbIMktRXv4cYrFC9XryeNFljy9e38nWIUGC3TgLD0";
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            Device.OpenUri(new Uri("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/visual"));
-        }
+       
+
+
     }
 }
