24240b18a1791d1aebd2162bee47922163801d3c
Nathaniel Nunes <manutdkid77@gmail.com>
2019-03-30 14:55:19 +0530

    Added seachbar and contacts and create buttons on Search

---
+++ VisualChallenge/VisualChallenge/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualChallenge.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Avatar { get; set; }
+    }
+}
